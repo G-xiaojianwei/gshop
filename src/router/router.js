f4e14d26d732889fcd613478d8654f56bca839ee
G-xiaojianwei <1510756488@qq.com>
2021-05-22 16:26:54 +0800

    添加了axios请求，下一步完成vuex配置

--- src/router/router.js
+++ src/router/router.js
@@ -5,6 +5,7 @@ import Msite from '../pages/Msite/Msite.vue'
 import Order from '../pages/Order/Order.vue'
 import Profile from '../pages/Profile/Profile.vue'
 import Search from '../pages/Search/Search.vue'
+import Login from '../pages/Login/Login.vue'
 
 Vue.use(VueRouter)
 
@@ -29,6 +30,10 @@ let router = new VueRouter({
         {
             path:'/',
             redirect:'/msite'
+        },
+        {
+            path:'/login',
+            component:Login
         }
     ]
 })
