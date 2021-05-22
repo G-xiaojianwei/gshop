1ae221cfb5422057eb9a56c4bfbab2352264167e
G-xiaojianwei <1510756488@qq.com>
2021-05-22 17:09:51 +0800

    更新了vuex

--- src/main.js
+++ src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 import router from '../src/router/router';
+import store from '../src/store/store'
 Vue.config.productionTip = false
 
 new Vue({
   render: h => h(App),
-  router
+  router,
+  store
 }).$mount('#app')
